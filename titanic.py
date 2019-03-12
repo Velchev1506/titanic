941204df32104086aa9dd0457348c3b365894496
Velchev Vanes_begin_real <31736576+Velchev1506@users.noreply.github.com>
2019-03-12 20:19:37 +0300

    Add files via upload

---
+++ titanic.py
@@ -0,0 +1,35 @@
+# импорт необходимых библиотек для работы
+import pandas as pd
+
+#data = None
+def get_nubmer_of_pass(data = None):
+	return 42
+
+male_int = get_nubmer_of_pass(data)
+female_int = get_nubmer_of_pass(data)
+
+# импорт необходимых библиотек для работы
+
+def percentage(perc, whole):
+	return (perc * 100) / whole
+
+def get_nubmer_of_pass(sex, data = None):
+    """
+        Какое количество мужчин и женщин ехало на параходе? 
+        Приведите два числа через пробел
+    """
+    sexratio = data.value_counts()
+
+    if sex == 'male':
+        return sexratio['male']
+    else:
+        return sexratio['female']
+
+data = pd.read_csv('train.csv', index_col='PassengerId')
+print(data['Sex'])
+male_int = get_nubmer_of_pass('male', data['Sex'] )
+female_int = get_nubmer_of_pass('female', data['Sex'])
+
+print (male_int, female_int)
+total_int = male_int + female_int
+print (round(percentage(male_int, total_int)), round(percentage(female_int, total_int)) )
